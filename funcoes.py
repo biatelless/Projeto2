6dc7dd18c1754d3364e8bd77e24310a32132786e
Ana Carolina Martinez Ferreira <anacmf1@al.insper.edu.br>
2025-04-25 09:25:18 -0300

    Fução Guardar dado

--- funcoes.py
+++ funcoes.py
@@ -8,3 +8,12 @@ def rolar_dados(n):
         lista.append(dado)
         i += 1 
     return lista
+
+
+
+def guardar_dado(dados, guardados, i):
+    guardados.append(dados[i])
+    del dados[i]
+    return [dados, guardados]
+
+    
