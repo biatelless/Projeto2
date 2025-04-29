2f0703bda3da52ade140b133804a7f8a995699ba
Beatriz Telles <beatriztf1@al.insper.edu.br>
2025-04-29 20:49:49 -0300

    Jogo (COM ERROS 3)

--- programa.py
+++ programa.py
@@ -10,7 +10,7 @@ cartela_de_pontos = {
         6:-1
     },
     'regra_avancada' : {
-        'sem_combinacao':1,
+        'sem_combinacao':-1,
         'quadra': -1,
         'full_house': -1,
         'sequencia_baixa': -1,
@@ -45,15 +45,17 @@ while jogada<12:
                     combinacao = input(">")
                 else:
                     cartela_de_pontos = funcoes.faz_jogada(guardados,combinacao,cartela_de_pontos)
-            if combinacao == "sem_combinacao" or combinacao == "quadra" or combinacao == "full_house" or combinacao == "sequencia_baixa" or combinacao == "sequencia_alta" or combinacao == "cinco_iguais":
+                    jogando = False
+            elif combinacao == "sem_combinacao" or combinacao == "quadra" or combinacao == "full_house" or combinacao == "sequencia_baixa" or combinacao == "sequencia_alta" or combinacao == "cinco_iguais":
                 if cartela_de_pontos["regra_avancada"][combinacao] != -1: 
                     print("Essa combinação já foi utilizada.")
                     combinacao = input(">")
                 else: 
                     cartela_de_pontos = funcoes.faz_jogada(guardados,combinacao,cartela_de_pontos)
+                    jogando = False
             elif combinacao != "1" and combinacao != "2" and combinacao != "3" and combinacao != "4" and combinacao != "5" and combinacao == "6" and combinacao != "sem_combinacao" and combinacao != "quadra" and combinacao != "full_house" and combinacao != "sequencia_baixa" and combinacao != "sequencia_alta" and combinacao != "cinco_iguais":
                 print("Essa combinação não existe.")
-                combinacao = input(">")
+                acao = int(input(">"))
 
         elif acao == 1: 
             print("Digite o índice do dado a ser guardado (0 a 4):")
@@ -80,13 +82,15 @@ while jogada<12:
             funcoes.imprime_cartela(cartela_de_pontos)
             
         if cartela_de_pontos["regra_simples"][1] != -1 and cartela_de_pontos["regra_simples"][2] != -1 and cartela_de_pontos["regra_simples"][3] != -1 and cartela_de_pontos["regra_simples"][4] != -1 and cartela_de_pontos["regra_simples"][5] != -1 and cartela_de_pontos["regra_simples"][6] != -1 and cartela_de_pontos["regra_avancada"]["sem_combinacao"] != -1 and cartela_de_pontos["regra_avancada"]["quadra"] != -1 and cartela_de_pontos["regra_avancada"]["full_house"] != -1 and cartela_de_pontos["regra_avancada"]["sequencia_baixa"] != -1 and cartela_de_pontos["regra_avancada"]["sequencia_alta"] != -1 and cartela_de_pontos["regra_avancada"]["cinco_iguais"] != -1:
-            pontuacao_final = cartela_de_pontos["regra_simples"][1] + cartela_de_pontos["regra_simples"][2] + cartela_de_pontos["regra_simples"][3] + cartela_de_pontos["regra_simples"][4] + cartela_de_pontos["regra_simples"][5] + cartela_de_pontos["regra_simples"][6] + cartela_de_pontos["regra_avancada"]["sem_combinacao"] + cartela_de_pontos["regra_avancada"]["quadra"] + cartela_de_pontos["regra_avancada"]["full_house"] + cartela_de_pontos["regra_avancada"]["sequencia_baixa"] + cartela_de_pontos["regra_avancada"]["sequencia_alta"] + cartela_de_pontos["regra_avancada"]["cinco_iguais"]
-            simples = cartela_de_pontos["regra_simples"][1] + cartela_de_pontos["regra_simples"][2] + cartela_de_pontos["regra_simples"][3] + cartela_de_pontos["regra_simples"][4] + cartela_de_pontos["regra_simples"][5] + cartela_de_pontos["regra_simples"][6]
-            if simples>63: 
-                pontuacao_final = pontuacao_final + 35
-            funcoes.imprime_cartela(cartela_de_pontos)
-            print("Pontuação final: {}".format(pontuacao_final))
-            jogando = False  
+            jogando = False
+            jogada = 12  
 
     jogada = jogada + 1 
+
+pontuacao_final = cartela_de_pontos["regra_simples"][1] + cartela_de_pontos["regra_simples"][2] + cartela_de_pontos["regra_simples"][3] + cartela_de_pontos["regra_simples"][4] + cartela_de_pontos["regra_simples"][5] + cartela_de_pontos["regra_simples"][6] + cartela_de_pontos["regra_avancada"]["sem_combinacao"] + cartela_de_pontos["regra_avancada"]["quadra"] + cartela_de_pontos["regra_avancada"]["full_house"] + cartela_de_pontos["regra_avancada"]["sequencia_baixa"] + cartela_de_pontos["regra_avancada"]["sequencia_alta"] + cartela_de_pontos["regra_avancada"]["cinco_iguais"]
+simples = cartela_de_pontos["regra_simples"][1] + cartela_de_pontos["regra_simples"][2] + cartela_de_pontos["regra_simples"][3] + cartela_de_pontos["regra_simples"][4] + cartela_de_pontos["regra_simples"][5] + cartela_de_pontos["regra_simples"][6]
+if simples>63: 
+    pontuacao_final = pontuacao_final + 35
+funcoes.imprime_cartela(cartela_de_pontos)
+print("Pontuação total: {}".format(pontuacao_final))
 
